e91257a2377b00eb89953ccab039a8b7defa7002
lackdoy <lackdoy@naver.com>
2017-08-28 05:12:07 +0900

    -문자열 식별하기

--- MyApplicationUI/app/src/main/java/com/example/myapplicationui/User_Interface/NavigationPopupActivity.java
+++ MyApplicationUI/app/src/main/java/com/example/myapplicationui/User_Interface/NavigationPopupActivity.java
@@ -11,7 +11,11 @@ import com.example.myapplicationui.Conection.whiteVoice;
 import com.example.myapplicationui.Function.TTSClass;
 import com.example.myapplicationui.R;
 
+import java.util.ArrayList;
+
 public class NavigationPopupActivity extends Activity {
+
+    ArrayList<String> signiture = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +24,30 @@ public class NavigationPopupActivity extends Activity {
         setContentView(R.layout.activity_navigation_popup);
         TextView dataDisplay = (TextView)findViewById(R.id.popUp);
 
+        signiture.add(new String("한경대학교"));
+        signiture.add(new String("한경대학교"));
+        signiture.add(new String("한경대학교"));
+        signiture.add(new String("안성로"));
+        signiture.add(new String("석정로"));
+        signiture.add(new String("안성로"));
+        signiture.add(new String("주작동"));
+        signiture.add(new String("홍석천"));
+        signiture.add(new String("임요한"));
+        signiture.add(new String("홍진호"));
+        signiture.add(new String("홍진호"));
+
         ((whiteVoice) getApplicationContext()).dataExplain = false;
         Intent getData = getIntent();
         double Second = getData.getIntExtra("fullDistance",1) / 0.8;
         int hour = (int)Second / 3600, min = (int)Second % 3600 / 60;
+        signiture = arrayOverlapCatcher(signiture);
+        String signitureList = "";
 
-        dataDisplay.setText("[입력]\n" + getData.getStringExtra("getVoiceString") + "\n\n[검색된 목적지]\n" + getData.getStringExtra("destinationmap") + "\n\n[총 거리]\n" + getData.getIntExtra("fullDistance",1) + "m\n\n[예상소요시간]\n" + hour + "시간 " + min +"분 예정");
+        for(int x = 0; x < signiture.size(); x++){
+            signitureList += "\n" + signiture.get(x) + ",";
+        }
+
+        dataDisplay.setText("[입력]\n" + getData.getStringExtra("getVoiceString") + "\n\n[검색된 목적지]\n" + getData.getStringExtra("destinationmap") + "\n\n[총 거리]\n" + getData.getIntExtra("fullDistance",1) + "m\n\n[예상소요시간]\n" + hour + "시간 " + min +"분 예정\n" + signitureList);
         TTSClass.Init(this, "현재 입력된, 목적지는," + getData.getStringExtra("getVoiceString") + " ,이며, 가장 근접하게, 검색된, 목적지는, " + getData.getStringExtra("destinationmap")+ ", 입니다.                                                   총 " + getData.getIntExtra("fullDistance",1) + ",미터,                                            거리이며, 약,  " + hour + ",시간," + min + ",분, 소요될 예정입니다.                                        ");
         Handler mHandler = new Handler();
         mHandler.postDelayed(new Runnable() {
@@ -33,5 +55,21 @@ public class NavigationPopupActivity extends Activity {
                 finish();
             }
         }, 15000);
+    }
+
+    public ArrayList<String> arrayOverlapCatcher(ArrayList<String> list){
+        boolean TF = true;
+        for(int i = 0; i < list.size();i++){
+            for(int z = i+1; z < list.size();){
+                TF = true;
+                if(list.get(i).equals(list.get(z))){
+                    list.remove(z);
+                    TF = false;
+                }
+                if(TF)
+                    z++;
+            }
+        }
+        return list;
     }
 }
