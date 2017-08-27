5800ed0d8a3b31f43892c596eb4fb0c7107c797f
박지찬 <qkrwlcks@naver.com>
2017-08-28 06:04:36 +0900

    메뉴액티비티  - 음성검색, 키보드검색 버튼 삭제  - 목적지검색 기능 추가(팝업윈도우 음성, 터치 선택)  - 사진분석 기능 추가(분석 후 팝업으로 사진, 분석결과, 다시듣기)  - 버튼이미지 수정

--- MyApplicationUI/app/src/main/java/com/example/myapplicationui/User_Interface/MenuActivity.java
+++ MyApplicationUI/app/src/main/java/com/example/myapplicationui/User_Interface/MenuActivity.java
@@ -3,28 +3,50 @@ package com.example.myapplicationui.User_Interface;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.speech.RecognizerIntent;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplicationui.CS.CSApi;
+import com.example.myapplicationui.CS.CSGetResult;
+import com.example.myapplicationui.CS.CSPostConfig;
+import com.example.myapplicationui.CS.CSPostResult;
 import com.example.myapplicationui.Conection.whiteVoice;
+import com.example.myapplicationui.Function.CameraActivity;
 import com.example.myapplicationui.Function.TTSClass;
 import com.example.myapplicationui.R;
 import com.tsengvn.typekit.TypekitContextWrapper;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static com.example.myapplicationui.User_Interface.NavigationActivity.HTTP_TRANSPORT;
+import static com.example.myapplicationui.User_Interface.NavigationActivity.JSON_FACTORY;
 
 
 public class MenuActivity extends Activity {
 
+    private static final String API_KEY = "vmCH8sw2l_2cXvsCx-wUKQ";
     private final static int PERMISSIONS_REQUEST_CODE = 100;
     private final int RESULT_SPEECH = 101;
 
@@ -76,14 +98,27 @@ public class MenuActivity extends Activity {
         }*/
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String path = ((whiteVoice)getApplicationContext()).tapPath;
+
+        if(path!=null){
+            File mFile = new File(path);
+            new ProcessCloudSight().execute(mFile);
+        }
+    }
+
     public void onClickFavorite(View view) {
         Intent intent = new Intent(this, FavoriteActivity.class);
         startActivity(intent);
     }
 
+
     public void onClickDestinationV(View view) {
-        TTSClass.Init(this, "목적지를 말하세요");
-        doSTT();
+        //TTSClass.Init(this, "목적지를 말하세요");
+        //doSTT();
         /*Intent intent = new Intent(this, STT_Activity.class);
         startActivity(intent);*/
         /*
@@ -91,15 +126,60 @@ public class MenuActivity extends Activity {
         intent.putExtra("request", 3);
         startActivity(intent);
         */
+
+
+        LayoutInflater inflater=getLayoutInflater();
+
+        //res폴더>>layout폴더>>dialog_addmember.xml 레이아웃 리소스 파일로 View 객체 생성
+        //Dialog의 listener에서 사용하기 위해 final로 참조변수 선언
+        final View dialogView= inflater.inflate(R.layout.activity_search_dialog, null);
+
+        //멤버의 세부내역 입력 Dialog 생성 및 보이기
+        AlertDialog.Builder buider= new AlertDialog.Builder(this); //AlertDialog.Builder 객체 생성
+        //buider.setTitle("Member Information"); //Dialog 제목
+        buider.setView(dialogView); //위에서 inflater가 만든 dialogView 객체 세팅 (Customize)
+
+        //설정한 값으로 AlertDialog 객체 생성
+        AlertDialog dialog=buider.create();
+        //Dialog의 바깥쪽을 터치했을 때 Dialog를 없앨지 설정
+        dialog.setCanceledOnTouchOutside(false);//없어지지 않도록 설정
+        //Dialog 보이기
+        dialog.show();
+
+        LinearLayout btnV = (LinearLayout)dialogView.findViewById(R.id.btnSearch_voice);
+        btnV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TTSClass.Init(getApplication(), "목적지를 말하세요");
+                doSTT();
+            }
+        });
+
+        LinearLayout btnT = (LinearLayout)dialogView.findViewById(R.id.btnSearch_touch);
+        btnT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), DestinationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
     public void onClickSetting(View view) {
         Intent intent = new Intent(this, SettingActivity.class);
         startActivity(intent);
     }
+    /*
 
     public void onClickDestinationT(View view){
         Intent intent = new Intent(this, DestinationActivity.class);
+        startActivity(intent);
+    }
+    */
+
+    public void onClickMenuTap(View v){
+        Intent intent = new Intent(this, CameraActivity.class);
         startActivity(intent);
     }
 
@@ -146,6 +226,152 @@ public class MenuActivity extends Activity {
             ((whiteVoice)getApplicationContext()).target = replace_sst;
             Intent intent = new Intent(this, NavigationActivity.class);
             startActivity(intent);
+        }
+    }
+
+    class ProcessCloudSight extends AsyncTask<File, Void, String> {
+
+        ProgressDialog dialog;
+        boolean flagCS = true;
+        String temp = null;
+        String ment;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            dialog= new ProgressDialog(MenuActivity.this, R.style.DialogCustom); //ProgressDialog 객체 생성
+            //dialog.setTitle("Progress");                   //ProgressDialog 제목
+            dialog.setMessage("분석중입니다...");             //ProgressDialog 메세지
+            //dialog.setCancelable(false);                      //종료금지
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER); //스피너형태의 ProgressDialog 스타일 설정
+            dialog.setCanceledOnTouchOutside(false); //ProgressDialog가 진행되는 동안 dialog의 바깥쪽을 눌러 종료하는 것을 금지
+            dialog.show(); //ProgressDialog 보여주기
+
+            // Dialog Cancle시 Event 받기
+            dialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+                public boolean onKey(DialogInterface dialog,
+                                     int keyCode, KeyEvent event) {
+                    if (keyCode == KeyEvent.KEYCODE_BACK) {
+                        dialog.dismiss();
+                        flagCS = false;
+                        return true;
+                    }
+                    return false;
+                }
+            });
+        }
+
+        @Override
+        protected String doInBackground(File... params) {
+            try {
+                CSApi api = new CSApi(
+                        HTTP_TRANSPORT,
+                        JSON_FACTORY,
+                        API_KEY
+                );
+
+                CSPostConfig imageToPost = CSPostConfig.newBuilder().withLanguage("ko-KR").withLocale("ko-KR")
+                        .withImage(params[0])
+                        //.withRemoteImageUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFhznR3_uOyGeN0luvThTUZRRWy1JPkqestP1TjePlJNXfq5g4")
+                        .build();
+
+                CSPostResult portResult = api.postImage(imageToPost);
+                Log.e("post", "Post result: " + portResult);
+                try {
+                    //Thread.sleep(30000);
+                    CSGetResult scoredResult = api.getImage(portResult);
+                    Log.e("scored", "Scored result: " + scoredResult);
+
+                    int i = 0;
+                    while (flagCS) {  //몇번이상 실패할경우 재캡쳐 코드 추가하기
+                        i++;
+                        if(i>=10) {
+                            temp = "다시 캡쳐하세요";
+                            break;
+                        }
+                        else {
+                            if (scoredResult.getStatus().equals("completed")) {
+                                temp = scoredResult.getName();
+                                Log.e("completed", "status : " + scoredResult.getStatus());
+                                Log.e("completed", "name : " + scoredResult.getName());
+                                break;
+                            } else {
+                                scoredResult = api.getImage(portResult);
+                                Thread.sleep(3500);
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.getCause();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return temp;
+        }
+
+        @Override
+        protected void onPostExecute(String name) {
+            super.onPostExecute(name);
+
+            dialog.dismiss();
+            if(name!=null)
+            {
+                TTSClass.Init(getApplication(), name);
+                ment = name;
+            }
+
+            LayoutInflater inflater=getLayoutInflater();
+
+            //res폴더>>layout폴더>>dialog_addmember.xml 레이아웃 리소스 파일로 View 객체 생성
+            //Dialog의 listener에서 사용하기 위해 final로 참조변수 선언
+            final View dialogView= inflater.inflate(R.layout.activity_tap, null);
+
+            //멤버의 세부내역 입력 Dialog 생성 및 보이기
+            AlertDialog.Builder buider= new AlertDialog.Builder(MenuActivity.this); //AlertDialog.Builder 객체 생성
+            //buider.setTitle("Member Information"); //Dialog 제목
+            buider.setView(dialogView); //위에서 inflater가 만든 dialogView 객체 세팅 (Customize)
+
+            //설정한 값으로 AlertDialog 객체 생성
+            final AlertDialog mDialog =buider.create();
+            //Dialog의 바깥쪽을 터치했을 때 Dialog를 없앨지 설정
+            mDialog.setCanceledOnTouchOutside(false);//없어지지 않도록 설정
+            //Dialog 보이기
+            mDialog.show();
+
+            File imgFile = new File(((whiteVoice)getApplicationContext()).tapPath);
+            ((whiteVoice)getApplicationContext()).tapPath = null;
+
+            if(imgFile.exists()){
+
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                ImageView imageView = (ImageView)dialogView.findViewById(R.id.imageViewTap);
+
+                imageView.setImageBitmap(myBitmap);
+            }
+
+            TextView viewMent = (TextView)dialogView.findViewById(R.id.textMentTap);
+            viewMent.setText(ment);
+
+            Button btnA = (Button)dialogView.findViewById(R.id.btnAgainTap);
+            btnA.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    TTSClass.Init(getApplication(), ment);
+                }
+            });
+
+            Button btnC = (Button)dialogView.findViewById(R.id.btnCloseTap);
+            btnC.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mDialog.dismiss();
+                    temp = null;
+                }
+            });
+
         }
     }
 
